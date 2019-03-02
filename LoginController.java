cb4215783cd8bdaf8df9549174763272c4f2f0e3
nagarajju <48145577+nagarajju@users.noreply.github.com>
2019-03-02 03:01:22 -0800

    Add files via upload

--- LoginController.java
+++ LoginController.java
@@ -3,15 +3,21 @@
  */
 package com.rbs.breach.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 /**
@@ -21,23 +27,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @PropertySource("classpath:application.properties")
-
-@ConfigurationProperties
 public class LoginController {
+
 	@Value("${spring.datasource.username}")
 	private String username1;
 	@Value("${spring.datasource.password}")
 	private String password2;
 	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	  public String init(@RequestParam String username,@RequestParam String Password) {
-	   if(username.equals(username1) && Password.equals("password2"))
-	   {
-		   return "login";
-	   }
-	   else
-	   {
-	    return "error";
-	   }
-	  }
+    @Autowired
+    LoginService service;
+
+    @GetMapping("/login")
+    public void loginmethod (@RequestParam String username,@RequestParam String password)
+    {
+    	System.out.println(username);
+    	if(username.equals(username1) && password.equals("password2"))
+ 	   {
+    	System.out.println("Intothat");	
+    		}
+ 	   }
 }
+
